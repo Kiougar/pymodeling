0cb343dc0d6e8797355aecd2e065b4156d1292c5
Georgios Tzourmpakis <kiougar@gmail.com>
2015-10-27 04:07:51 +0200

    Some PEP8 formatting changes

--- modeling/validation.py
+++ modeling/validation.py
@@ -1,14 +1,15 @@
-'''
+"""
 Created on Oct 24, 2014
 
 @author: vsam
-'''
+"""
 from contextlib import contextmanager, ContextDecorator
 from sys import exc_info
 from traceback import extract_tb, format_exception_only, format_exception
 from os.path import basename
 import logging
 import threading
+
 
 ######################
 # Inline validation
@@ -19,53 +20,65 @@ import threading
 # N.B. may need this to be thread-local
 
 class CheckException(Exception):
-    '''
+    """
     Base class for checking exceptions.
-    '''
+    """
     pass
+
     def __init__(self, scope=None):
         self.scope = scope
+
     def __repr__(self):
         sc = "<%s>" % self.scope if self.scope else ""
         return "%s(%s)" % (self.__class__.__name__, sc)
 
+
 class CheckFail(CheckException):
-    'Raised by fail(...)'
+    """Raised by fail(...)"""
     pass
 
+
 class CheckFatal(CheckException):
-    'Raised by fatal(...)'
+    """Raised by fatal(...)"""
     pass
 
 
 class ScopeStack:
-    '''
+    """
     A simple implementation of the scope stack. Suitable only for non-threaded
     environments. Use ThreadLocalScopeStack for more thread-local scopes.
-    '''
+    """
+
     def __init__(self):
         self.stack = []
+
     def top(self):
         return self.stack[-1]
+
     def push(self, scope):
         pos = len(self.stack)
         self.stack.append(scope)
         scope.stack_positions.append(pos)
+
     def pop(self):
         scope = self.stack.pop()
         pos = scope.stack_positions.pop()
         assert pos == len(self.stack)
+
     def __getitem__(self, item):
         return self.stack[item]
+
     def __bool__(self):
         return bool(self.stack)
-    def __len__(self): 
+
+    def __len__(self):
         return len(self.stack)
 
+
 class ThreadLocalScopeStack(threading.local, ScopeStack):
-    '''
+    """
     A thread-local scope stack
-    '''
+    """
     pass
 
 
@@ -73,11 +86,11 @@ scope_stack = ThreadLocalScopeStack()
 
 
 class CheckScope(ContextDecorator):
-    '''
-    Base class for Process and Context scopes. 
+    """
+    Base class for Process and Context scopes.
 
     Scopes are contextually accessed objects which implement operation
-    tracing. There are two types of scopes: Process and Context. Scopes 
+    tracing. There are two types of scopes: Process and Context. Scopes
     are nested within one another.
 
     A Process scope is defined by a 'with' statement:
@@ -100,37 +113,37 @@ class CheckScope(ContextDecorator):
     appear in messages, (c) suppress the propagation of some exceptions.
 
     The attributes passed at context construction are passed to the logger when
-    messages are logged, via a LoggerAdapter. 
+    messages are logged, via a LoggerAdapter.
 
     Contexts are fine-grained and one may have many short ones.
 
-    '''
+    """
+
     def __init__(self, name=None):
-        '''
+        """
         Initialize scope, possibly with a name
-        '''
-        self.name = name       # name of the scope
-        self.success = True    # record success
+        """
+        self.name = name  # name of the scope
+        self.success = True  # record success
         self.logger_adapter = None  # the adapter to this logger, or None
-        self.suppression = set()    # the set of exception classes to suppress
+        self.suppression = set()  # the set of exception classes to suppress
         self.stack_positions = []  # The positions this Scope appears in the stack
 
     __std_logger = None
 
     def suppress(self, exc_type):
-        '''
+        """
         Add an expression type to the suppression list. If an expression of
-        this type is caught, it will be logged and then suppressed. 
+        this type is caught, it will be logged and then suppressed.
 
         Note that adding Exception suppresses most, but not all exceptions. Add
         BaseException to suppress everything.
-        '''
+        """
         self.suppression.add(exc_type)
-
 
     @property
     def logger(self):
-        '''The current logger.'''
+        """The current logger."""
         if hasattr(self, 'own_logger'):
             # return your own logger
             return self.own_logger
@@ -141,13 +154,13 @@ class CheckScope(ContextDecorator):
 
     @property
     def log(self):
-        '''A logger or adapter to use inside this scope.'''
+        """A logger or adapter to use inside this scope."""
         return self.logger_adapter if self.logger_adapter is not None else self.logger
 
     @property
     def parent(self):
         if self.stack_positions and self.stack_positions[0]:
-            return scope_stack[self.stack_positions[0]-1]
+            return scope_stack[self.stack_positions[0] - 1]
         else:
             return None
 
@@ -161,7 +174,6 @@ class CheckScope(ContextDecorator):
         else:
             return None
 
-
     def __enter__(self):
         # push yourself on the stack (check that if the stack is empty then we must be
         scope_stack.push(self)
@@ -170,21 +182,19 @@ class CheckScope(ContextDecorator):
     suppress_types = ()
 
     def catches(self, exc_type, exc):
-        '''
+        """
         Return True if the passed exception type and value should be caught at
         this scope, or propagated down the scope stack.
-        '''
+        """
         return (
-                (exc_type in self.suppress_types
-                    and 
-                    (
-                        exc.scope is None or 
-                        exc.scope == self.name or 
-                        exc.scope is self
-                    ))
-            or 
-                any(isinstance(exc, exct) for exct in self.suppression)
-            )
+            (exc_type in self.suppress_types and
+             (
+                 exc.scope is None or
+                 exc.scope == self.name or
+                 exc.scope is self
+             )) or
+            any(isinstance(exc, exct) for exct in self.suppression)
+        )
 
     def __exit__(self, exc_type, exc, tb):
         try:
@@ -193,45 +203,49 @@ class CheckScope(ContextDecorator):
                 catch = self.catches(exc_type, exc)
 
                 if catch and exc_type not in self.suppress_types:
-                    self.log.exception("An unexpected error occurred:\n%s", 
-                        ''.join(format_exception(exc_type, exc, tb)))
+                    self.log.exception("An unexpected error occurred:\n%s",
+                                       ''.join(format_exception(exc_type, exc, tb)))
 
                 return catch
         finally:
             # just pop from the stack
-            if self.parent: self.parent.success = self.parent.success and self.success
+            if self.parent:
+                self.parent.success = self.parent.success and self.success
             assert scope_stack[-1] is self
+            # TODO function pop doesn't return anything...
+            # noinspection PyNoneFunctionAssignment,PyUnusedLocal
             me = scope_stack.pop()
 
 
 class Process(CheckScope):
-    '''
+    """
     A process is a scope which may contain a logger for subscopes.
     The top-level scope has to be a Process.
 
     See the documentation of class CheckScope for details.
-    '''
-    suppress_types = (CheckFatal,CheckFail)
+    """
+    suppress_types = (CheckFatal, CheckFail)
 
     def __init__(self, name=None, logger=None):
         super().__init__(name)
-        if logger is not None:    
+        if logger is not None:
             if isinstance(logger, logging.Logger):
                 self.logger = logger
             elif isinstance(logger, str):
                 self.logger = logging.getLogger(logger)
-            else: 
+            else:
                 assert False
             self.logger.setLevel(logging.INFO)
             self.logger.propagate = False
-
+        # g Added this to stop pycharm from complaining that we define own_logger outside __init__
+        self.own_logger = None
         self.handlers = set()
 
-    def addScopeHandler(self, handler):
-        '''
+    def add_scope_handler(self, handler):
+        """
         Add a handler to the underlying logger (which may be the parent's)
         logger. This handler will be removed when the scope exits
-        '''
+        """
         self.handlers.add(handler)
         if self.stack_positions:
             self.logger.addHandler(handler)
@@ -249,25 +263,26 @@ class Process(CheckScope):
             for handler in self.handlers:
                 self.logger.removeHandler(handler)
 
+    # TODO this function overrides CheckScope.logger, where the latter simply returns the logger, while this sets it
     @CheckScope.logger.setter
     def logger(self, logger):
         self.own_logger = logger
 
 
 class Context(CheckScope):
-    '''
-    Contexts provide extra arguments to messages, via 
+    """
+    Contexts provide extra arguments to messages, via
     keyword arguments given at the constructor.
 
     See the documentation of class CheckScope for details.
-    '''
+    """
     suppress_types = (CheckFail,)
 
     def __init__(self, name=None, **extra):
         super().__init__(name)
         self.extra_own = extra if extra else {}
 
-    def add(**kwargs):
+    def add(self, **kwargs):
         self.extra.update(kwargs)
 
     def __enter__(self):
@@ -275,7 +290,7 @@ class Context(CheckScope):
 
         # go down the stack and create the extras for the logger adapter
         pc = self.parent
-        if pc is not None and hasattr(pc,'extra'):
+        if pc is not None and hasattr(pc, 'extra'):
             extra = dict(pc.extra)
         else:
             extra = {}
@@ -297,11 +312,12 @@ def _check_scope(kwargs):
     scope = kwargs['scope'] if 'scope' in kwargs else None
     return scope
 
+
 def _out_of_context(msg, args, kwargs):
-    "Called when fail() or fatal() are called without context"
-    if 'ooc' in kwargs\
-        and isinstance(kwargs['ooc'], type)\
-        and issubclass(kwargs['ooc'], BaseException):
+    """Called when fail() or fatal() are called without context"""
+    if 'ooc' in kwargs \
+            and isinstance(kwargs['ooc'], type) \
+            and issubclass(kwargs['ooc'], BaseException):
         raise kwargs['ooc'](msg, args, kwargs)
     else:
         raise RuntimeError(msg, args, kwargs)
@@ -320,60 +336,64 @@ def success():
 
 
 def fail(msg=None, *args, **kwargs):
-    '''
+    """
     Raise to abort this context or process, or the context named
     in the keyword argument 'scope'.
     If message evaluates to False, do not issue a message.
     All the scopes upt to the top-level process are marked as failed.
 
     E.g. fail('bad situation in %s', place, scope='myloc')
-    '''
+    """
     if not scope_stack:
         return _out_of_context(msg, args, kwargs)
     if msg:
         scope_stack[-1].log.error(msg, *args, extra=kwargs)
     raise CheckFail(scope=_check_scope(kwargs))
 
+
 def fatal(msg=None, *args, **kwargs):
-    '''
+    """
     Raise to abort the nearest process, or the process named
     in the keyword argument 'scope'.
     If message evaluates to False, do not issue a message.
     All the scopes upt to the top-level process are marked as failed.
 
-    E.g. fatal('bad situation in %s', place, scope='myloc')    
-    '''
+    E.g. fatal('bad situation in %s', place, scope='myloc')
+    """
     if not scope_stack:
         return _out_of_context(msg, args, kwargs)
     if msg:
         scope_stack[-1].log.critical(msg, *args, extra=kwargs)
     raise CheckFatal(scope=_check_scope(kwargs))
 
+
 def inform(msg, *args, **kwargs):
-    '''
+    """
     Issue an informational message and continue.
-    '''
+    """
     if not scope_stack:
         logging.getLogger().info(msg, args, extra=kwargs)
         return
     scope_stack[-1].log.info(msg, *args, extra=kwargs)
-    
+
+
 def warn(msg, *args, **kwargs):
-    '''
+    """
     Issue a warning message and continue.
-    '''
+    """
     if not scope_stack:
         logging.getLogger().warning(msg, args, extra=kwargs)
         return
     scope_stack[-1].log.warning(msg, *args, extra=kwargs)
 
+
 def snafu(msg=None, *args, **kwargs):
-    '''
-    Issue an error message and continue. 
+    """
+    Issue an error message and continue.
     If message evaluates to False, do not issue a message.
     All scopes up to the top-level are marked as failed.
     However, processing continues normally.
-    '''
+    """
     if not scope_stack:
         if msg:
             logging.getLogger().error(msg, args, extra=kwargs)
@@ -386,7 +406,7 @@ def snafu(msg=None, *args, **kwargs):
 #
 #  Model validation
 #
-    
+
 class Validation:
     """This class is used to log a model validation.
     
@@ -422,67 +442,70 @@ class Validation:
                 ...    
     
     """
-    
-    INDENT_SIZE=4  # number of spaces per indent
+
+    INDENT_SIZE = 4  # number of spaces per indent
 
     # detail levels
-    SUCCESS=0
-    INFO=10
-    FAIL=20
-    SECTION=30
-    EXCEPTION=40
+    SUCCESS = 0
+    INFO = 10
+    FAIL = 20
+    SECTION = 30
+    EXCEPTION = 40
     # reserved
-    QUIET=100
-    
+    QUIET = 100
+
     class Abort(Exception):
         pass
+
     class MaxFailures(Exception):
         pass
-    
-    def __init__(self, outfile=None, detail=0, max_failures = 1000):
+
+    def __init__(self, outfile=None, detail=0, max_failures=1000):
         # counters
-        self.failures = 0                   # number of failures
-        self.max_failures = max_failures    # max number of failures to tolerate
-        self.enter = 0                      # number of calls to __enter__
+        self.failures = 0  # number of failures
+        self.max_failures = max_failures  # max number of failures to tolerate
+        self.enter = 0  # number of calls to __enter__
         self.section_failures = []
 
         # output control
-        self.detail = detail                # the level of detail to record
-        self.level = 0                      # current indentation level
-        self.outfile = outfile              # the output file
-        self.exc_output_limit = 1           # number of lines per exception
-    
+        self.detail = detail  # the level of detail to record
+        self.level = 0  # current indentation level
+        self.outfile = outfile  # the output file
+        self.exc_output_limit = 1  # number of lines per exception
+
         if self.outfile is None:
-            self.detail = self.QUIET      
-    
+            self.detail = self.QUIET
+
     def failure(self):
         self.failures += 1
         if self.section_failures:
             self.section_failures[-1] += 1
-        if self.failures>=self.max_failures:
+        if self.failures >= self.max_failures:
             raise Validation.MaxFailures
-        
+
     def output(self, header, msg, *args, **kwargs):
-        if self.outfile is None: return
+        if self.outfile is None:
+            return
         out = msg.format(*args, **kwargs)
         # split into lines
         lines = out.split('\n')
         for line in lines:
-            if not line: continue
-            print(" "*(self.INDENT_SIZE*self.level),
+            if not line:
+                continue
+            print(" " * (self.INDENT_SIZE * self.level),
                   sep='', end='', file=self.outfile, flush=True)
-            print(header, line,  
-                    file=self.outfile, flush=True)
-        
+            print(header, line,
+                  file=self.outfile, flush=True)
+
     def fail(self, msg, *args, **kwargs):
         self.failure()
-        if msg and self.detail <= self.FAIL: 
+        if msg and self.detail <= self.FAIL:
             self.output("FAIL:", msg, *args, **kwargs)
 
     def exception(self, etype, evalue, etb):
         self.failure()
         if self.outfile and self.detail <= self.EXCEPTION:
-            #for line in format_exception(type, value, tb, self.exc_output_limit):
+            # for line in format_exception(type, value, tb, self.exc_output_limit):
             #    self.output("EXCEPTION:", line, end='')
             for frame in extract_tb(etb):
                 fname, fline, func, _ = frame
@@ -490,19 +513,19 @@ class Validation:
                 self.output("EXCEPTION:", "{0}({1}): {2}", fbase, fline, func)
             for line in format_exception_only(etype, evalue):
                 self.output("EXCEPTION:", line)
-    
+
     def success(self, msg, *args, **kwargs):
-        if msg and self.detail <= self.SUCCESS: 
+        if msg and self.detail <= self.SUCCESS:
             self.output("SUCCESS:", msg, *args, **kwargs)
 
     def info(self, msg, *args, **kwargs):
-        if msg and self.detail <= self.INFO: 
+        if msg and self.detail <= self.INFO:
             self.output("INFO:", msg, *args, **kwargs)
 
     def __enter__(self):
         self.enter += 1
         return self
-    
+
     def __exit__(self, exc_type, exc_val, exc_tb):
         # Propagate Abort or MaxFailures immediately.
         # Other exceptions are swallowed
@@ -510,30 +533,29 @@ class Validation:
         self.enter -= 1
         if exc_type is AssertionError:
             return False
-        
+
         if exc_type is None:
             return True
 
-        if exc_type is Validation.Abort or self.failures>=self.max_failures:
-            if self.failures==0 and self.enter==0: self.failures+=1
-            return self.enter==0  # re-raise exception except if at the top-level
+        if exc_type is Validation.Abort or self.failures >= self.max_failures:
+            if self.failures == 0 and self.enter == 0:
+                self.failures += 1
+            return self.enter == 0  # re-raise exception except if at the top-level
 
         try:
             if exc_type is not Validation.MaxFailures:
                 self.exception(exc_type, exc_val, exc_tb)
         except Validation.MaxFailures:
-            return self.enter==0 # newly raised MaxFailures
+            return self.enter == 0  # newly raised MaxFailures
         else:
-            return True # swallow other 
+            return True  # swallow other
 
-
-        
     def __call__(self, check, label=None, *args, **kwargs):
         if check:
             self.success(label, *args, **kwargs)
         else:
             self.fail(label, *args, **kwargs)
-        
+
     @contextmanager
     def section(self, msg, *args, **kwargs):
         """Only to be called by a context manager, this starts a new section in the
@@ -543,15 +565,16 @@ class Validation:
             self.output("SECTION:", msg, *args, **kwargs)
         self.level += 1
         self.section_failures.append(0)
+        # noinspection PyBroadException
         try:
             yield self
-        except (AssertionError,Validation.MaxFailures,Validation.Abort):
+        except (AssertionError, Validation.MaxFailures, Validation.Abort):
             raise
         except Exception:
             exc_type, exc_value, exc_tb = exc_info()
             self.exception(exc_type, exc_value, exc_tb)
         finally:
-            if msg and self.detail <= self.SECTION and self.section_failures[-1]>0:
+            if msg and self.detail <= self.SECTION and self.section_failures[-1] > 0:
                 self.output("FAIL:", "Section encountered {0} failures.", self.section_failures[-1])
             self.level -= 1
             fcount = self.section_failures.pop()
@@ -559,12 +582,10 @@ class Validation:
             if self.section_failures:
                 self.section_failures[-1] += fcount
 
-
     def passed(self):
-        return self.failures==0
+        return self.failures == 0
+
     def passed_section(self):
         if self.section_failures:
-            return self.section_failures[-1]==0
+            return self.section_failures[-1] == 0
         return True
-
-
